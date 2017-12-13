5c94e21b3b00b670854c2f59d48c6574a5b87e56
Pratik Thorat <webmaster@thorat.tech>
2017-12-13 20:38:36 +0530

    index completed

--- js/index.js
+++ js/index.js
@@ -1,70 +1,73 @@
-// const database = require('./js/database');
+const database = require('./js/database');
 
-// window.onload = function() {
+window.onload = function() {
 
-//   // Populate the table
-//   populateTable();
-//   //tablecreate();
+    // Populate the table
+    populateTable();
 
-//   // Add the add button click event
-//   document.getElementById('add').addEventListener('click', () => {
+    //   // Add the add button click event
+    //   document.getElementById('add').addEventListener('click', () => {
 
-//     // Retrieve the input fields
-//     var firstname = document.getElementById('firstname');
-//     var lastname = document.getElementById('lastname');
+    //     // Retrieve the input fields
+    //     var firstname = document.getElementById('firstname');
+    //     var lastname = document.getElementById('lastname');
 
-//     // Save the person in the database
-//     database.addPerson(firstname.value, lastname.value);
+    //     // Save the person in the database
+    //     database.addPerson(firstname.value, lastname.value);
 
-//     // Reset the input fields
-//     firstname.value = '';
-//     lastname.value = '';
+    //     // Reset the input fields
+    //     firstname.value = '';
+    //     lastname.value = '';
 
-//     // Repopulate the table
-//     populateTable();
-//   });
-// }
+    //     // Repopulate the table
+    //     populateTable();
+    //   });
+    // }
 
-// // Populates the persons table
-// function populateTable() {
+    // // Populates the persons table
+    function populateTable() {
 
-//  // selectuser();
+        //  // selectuser();
 
-//   // Retrieve the persons
-//   database.getPersons(function(persons) {
+        // Retrieve the persons
+        database.getallrows(function(persons) {
+            console.log(persons);
+            // Generate the table body
+            var tableBody = '';
+            for (i = 0; i < persons.length; i++) {
+                var data = persons[i].img;
 
-//     // Generate the table body
-//     var tableBody = '';
-//     for (i = 0; i < persons.length; i++) {
-//       tableBody += '<tr>';
-//       tableBody += '  <td>' + persons[i].accountno + '</td>';
-//       tableBody += '  <td>' + persons[i].name + '</td>';
-//       tableBody += '  <td><input type="button" value="Delete" onclick="deletePerson(\'' + persons[i].name + '\')"></td>'
-//       tableBody += '</tr>';
+                // Convert the string to bytes
+                var bytes = new Uint8Array(data.length / 2);
 
-//     }
+                for (var j = 0; j < data.length; j += 2) {
+                    bytes[j / 2] = parseInt(data.substring(j, j + 2), /* base = */ 16);
+                }
 
-//     // Fill the table content
-//     document.getElementById('tablebody').innerHTML = tableBody;
-//   });
-// }
+                // Make a Blob from the bytes
+                var blob = new Blob([bytes], { type: 'image/bmp' });
 
-// // Deletes a person
-// function deletePerson(id) {
+                // Use createObjectURL to make a URL for the blob
+                tableBody += '<div class = "col-md-4" ><a href = "userdetails.html?' + persons[i].num + '" >';
+                tableBody += '<div class="team-container">';
+                tableBody += '<img class="profile" src="' + URL.createObjectURL(blob) + '"> <h1>' + persons[i].name + '</h1>';
+                tableBody += ' <h3>Last Visit:' + persons[i].last + '</h3>';
+                tableBody += '<hr></hr>';
+                tableBody += '<div class="col-md-4 weight red"><h3>' + persons[i].iweight + '</h3><h4>Initial</h4></div><div class="col-md-4 weight blue"><h2>' + persons[i].cweight + '<sup> <i class="fa fa-chevron-down" aria-hidden="true"></i></sup></h2><h4>Current</h4></div><div class = "col-md-4 weight green"><h3>' + persons[i].tweight + '</h3><h4>target</h4></div></div></a></div>';
+            }
 
-//   alert("reach")
-//   // Delete the person from the database
-//   database.deletePerson(id);
+            // Fill the table content
+            document.getElementById('users').innerHTML = tableBody;
+        });
+    }
 
-//   // Repopulate the table
-//   populateTable();
-// }
+    // // Deletes a person
+    // function deletePerson(id) {
 
-// function tablecreate(){
-//   database.createtable();
-// }
+    //   alert("reach")
+    //   // Delete the person from the database
+    //   database.deletePerson(id);
 
-// // function selectuser(){
-// //  facts= database.selectall();
-// //   alert(facts);
-// // }
+    //   // Repopulate the table
+    //   populateTable();
+}
