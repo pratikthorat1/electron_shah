0811e2d21218eea8df78c59e0532edfc82a5b09a
Pratik Thorat <webmaster@thorat.tech>
2017-12-14 00:10:11 +0530

    just final

--- js/index.js
+++ js/index.js
@@ -7,23 +7,14 @@ window.onload = function() {
 
     //   // Add the add button click event
     document.getElementById('add').addEventListener('click', () => {
-        var imageToBlob = require('image-to-blob'),
-            foo = document.getElementById('image'),
-            DOMURL = window.URL || window.webkitURL || window;
 
-        function appendBlob(err, blob) {
-            if (err) {
-                console.error(err);
-                return;
-            }
-
-            console.log(blob);
-            var img = document.createElement('img');
-            img.src = DOMURL.createObjectURL(blob);
-            document.body.appendChild(img);
-        }
-
-        imageToBlob(foo, appendBlob);
+        var img = document.getElementById("check");
+        blobUtil.imgSrcToBlob(img.src).then(function(blob) {
+            var i = blob;
+            console.log(i);
+        }).catch(function(err) {
+            console.log(err);
+        });
         //     // Retrieve the input fields
         //     var firstname = document.getElementById('firstname');
         //     var lastname = document.getElementById('lastname');
@@ -38,7 +29,7 @@ window.onload = function() {
         //     // Repopulate the table
         //     populateTable();
     });
-
+    // }
 
     // // Populates the persons table
     function populateTable() {
@@ -54,19 +45,19 @@ window.onload = function() {
                 var data = persons[i].img;
 
                 // Convert the string to bytes
-                var bytes = new Uint8Array(data.length / 2);
+                // var bytes = new Uint8Array(data.length / 2);
 
-                for (var j = 0; j < data.length; j += 2) {
-                    bytes[j / 2] = parseInt(data.substring(j, j + 2), /* base = */ 16);
-                }
+                // for (var j = 0; j < data.length; j += 2) {
+                //     bytes[j / 2] = parseInt(data.substring(j, j + 2), /* base = */ 16);
+                // }
 
-                // Make a Blob from the bytes
-                var blob = new Blob([bytes], { type: 'image/bmp' });
-
+                // // Make a Blob from the bytes
+                // var blob = new Blob([bytes], { type: 'image/bmp' });
+                var bloburl = blobUtil.createObjectURL(data);
                 // Use createObjectURL to make a URL for the blob
                 tableBody += '<div class = "col-md-4" ><a href = "userdetails.html?' + persons[i].num + '" >';
                 tableBody += '<div class="team-container">';
-                tableBody += '<img class="profile" src="' + URL.createObjectURL(blob) + '"> <h1>' + persons[i].name + '</h1>';
+                tableBody += '<img id="check" class="profile" src="' + bloburl + '"> <h1>' + persons[i].name + '</h1>';
                 tableBody += ' <h3>Last Visit:' + persons[i].last + '</h3>';
                 tableBody += '<hr></hr>';
                 tableBody += '<div class="col-md-4 weight red"><h3>' + persons[i].iweight + '</h3><h4>Initial</h4></div><div class="col-md-4 weight blue"><h2>' + persons[i].cweight + '<sup> <i class="fa fa-chevron-down" aria-hidden="true"></i></sup></h2><h4>Current</h4></div><div class = "col-md-4 weight green"><h3>' + persons[i].tweight + '</h3><h4>target</h4></div></div></a></div>';
