958b075d9dc02e82e8ff4d4f2576bceeb879d0d7
Pratik Thorat <webmaster@thorat.tech>
2017-12-14 13:38:36 +0530

    data

--- js/index.js
+++ js/index.js
@@ -45,22 +45,22 @@ window.onload = function() {
                 var data = persons[i].img;
 
                 // Convert the string to bytes
-                // var bytes = new Uint8Array(data.length / 2);
+                var bytes = new Uint8Array(data.length / 2);
 
-                // for (var j = 0; j < data.length; j += 2) {
-                //     bytes[j / 2] = parseInt(data.substring(j, j + 2), /* base = */ 16);
-                // }
+                for (var j = 0; j < data.length; j += 2) {
+                    bytes[j / 2] = parseInt(data.substring(j, j + 2), /* base = */ 16);
+                }
 
-                // // Make a Blob from the bytes
-                // var blob = new Blob([bytes], { type: 'image/bmp' });
-                var bloburl = blobUtil.createObjectURL(data);
+                // Make a Blob from the bytes
+                var blob = new Blob([bytes], { type: 'image/bmp' });
+
                 // Use createObjectURL to make a URL for the blob
                 tableBody += '<div class = "col-md-4" ><a href = "userdetails.html?' + persons[i].num + '" >';
                 tableBody += '<div class="team-container">';
-                tableBody += '<img id="check" class="profile" src="' + bloburl + '"> <h1>' + persons[i].name + '</h1>';
+                tableBody += '<img id="check" class="profile" src="' + URL.createObjectURL(blob) + '"> <h1>' + persons[i].name + '</h1>';
                 tableBody += ' <h3>Last Visit:' + persons[i].last + '</h3>';
                 tableBody += '<hr></hr>';
-                tableBody += '<div class="col-md-4 weight red"><h3>' + persons[i].iweight + '</h3><h4>Initial</h4></div><div class="col-md-4 weight blue"><h2>' + persons[i].cweight + '<sup> <i class="fa fa-chevron-down" aria-hidden="true"></i></sup></h2><h4>Current</h4></div><div class = "col-md-4 weight green"><h3>' + persons[i].tweight + '</h3><h4>target</h4></div></div></a></div>';
+                tableBody += '<div class="col-md-4 weight red"><h3>' + persons[i].iweight + 'kg</h3><h4>Initial</h4></div><div class="col-md-4 weight blue"><h2>' + persons[i].cweight + 'kg<sup> <i class="fa fa-chevron-down" aria-hidden="true"></i></sup></h2><h4>Current</h4></div><div class = "col-md-4 weight green"><h3>' + persons[i].tweight + 'kg</h3><h4>target</h4></div></div></a></div>';
             }
 
             // Fill the table content
