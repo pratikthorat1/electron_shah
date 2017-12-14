80f1c96ca6596003602002c19a5b594a1d79cfc1
Pratik Thorat <webmaster@thorat.tech>
2017-12-14 22:47:28 +0530

    final work office

--- js/database.js
+++ js/database.js
@@ -57,7 +57,7 @@ exports.getallrows = function(fna) {
     // db.all("select up.memberno as memberno,it.img1 as img,up.person_nm as name,pt.prog_date as last,up.person_weight as iweight,pt.prog_weight as cweight,up.person_weight as tweight from user_profile up,progress_table pt,images_table it", function(err, rows) {
     //     fnc(rows);
     // });
-    var sql = `select up.memberno as num,it.img1 as img,up.person_nm as name,pt.prog_date as last,up.person_weight as iweight,pt.prog_weight as cweight,up.person_weight as tweight from user_profile up,progress_table pt,images_table it`;
+    var sql = `select up.memberno as num,it.img1 as img,up.person_nm as name,pt.prog_date as last,up.person_weight as iweight,pt.prog_weight as cweight,up.person_weight as tweight from user_profile up,progress_table pt,images_table it group by up.memberno`;
 
     db.all(sql, (err, rows) => {
         if (err) {
